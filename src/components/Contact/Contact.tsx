6c3a4924a75391c683ffb46d7e538d23a5e56ac2
thatGui <gui@thatgui.com>
2022-07-09 22:55:01 +0100

    content component added + css

--- src/components/Contact/Contact.tsx
+++ src/components/Contact/Contact.tsx
@@ -26,9 +26,10 @@ const Contact: React.FC<IChangable> = (props) => {
             <div className="contacttxt">
                 <div className="ctline"></div>
                 <h3>COLLABORATE</h3>
-                <h3>WITH US</h3>
+                <span>WITH US</span>
                 <div className="txtp">
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore</p>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                    <p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore</p>
                 </div>
             </div>
            
@@ -37,36 +38,40 @@ const Contact: React.FC<IChangable> = (props) => {
                 <img src={dets2} id='dts2' alt="" />
                 
                 <form>
-                <label> NAME: <input type="text" name="name" /></label>
+           
+                <label> NAME: <br/><input type="text" name="name" placeholder='Name' /></label>
+               
+             
+                <label> SURNAME: <br/><input type="text" name="surname" placeholder='Surname' /></label>
+               
+             
+                <label> PHONE: <br/><input type="number" name="phone" placeholder='Phone'/></label>
+               
+         
 
-                <label> SURNAME: <input type="text" name="surname" /></label>
+                <label> EMAIL ADDRESS: <br/><input type="email" name="email" placeholder='Email'/></label>
 
-                <label> PHONE: <input type="number" name="phone" /></label>
-
-                <label> EMAIL ADDRESS: <input type="email" name="email" /></label>
-
-                <label> EXPERIENCE:
+                <label> EXPERIENCE:<br/>
                 <select>
-                    <option value="grapefruit">Grapefruit</option>
+                    <option>Select</option>
                     <option value="lime">Lime</option>
-                    <option selected value="coconut">Coconut</option>
+                    <option value="coconut">Coconut</option>
                     <option value="mango">Mango</option>
                 </select>
                 </label>
 
                 
-                <label> WHERE DID YOU HEAR ABOUT US?: 
+                <label> WHERE DID YOU HEAR ABOUT US?<br/>
                 <select>
+                    <option>Select</option>
+                    <option value="mango">Mango</option>
                     <option value="grapefruit">Grapefruit</option>
                     <option value="lime">Lime</option>
-                    <option selected value="coconut">Coconut</option>
-                    <option value="mango">Mango</option>
                 </select>
                 </label>
 
-                <label> after 2nd drop down: <input type="text" name="name" /></label>
 
-                <input type="submit" value="SEND" />
+                <input type="submit" value="SEND" className='formbtn'/>
 
                 </form>
 
